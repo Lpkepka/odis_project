0f685e784b11a16ad08bca626d5fc5ef6f0c1e87
Łukasz Kępka <lukaszkepka97@gmail.com>
2021-04-21 21:49:00 +0200

    removed print

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ while True:
     event = splitLine[2]
     text = ''.join(splitLine[3:]).replace(':', '').replace('.', '')
     formattedDate = '2021-{}-{} {}'.format(day.split('/')[0], day.split('/')[1], hour)
-    print(day, hour, event, formattedDate, text)
     sql = 'INSERT INTO Logs(message, event_type, server_id, date) VALUES(?, ?, 1, ?);'
     c.execute(sql, [text, event, formattedDate])
 file1.close()
