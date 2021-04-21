c2ab189ecaad0c853011e098962d0b088f5ffd67
Łukasz Kępka <lukaszkepka97@gmail.com>
2021-04-21 21:08:54 +0200

    Added example logs

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import sqlite3
 conn = sqlite3.connect('Logs.db')
 
 c = conn.cursor()
-c.execute('''CREATE TABLE LOGS
+c.execute('''CREATE TABLE IF NOT EXISTS LOGS
              ([generated_id] INTEGER PRIMARY KEY,[Text] text, [Server_ID] integer, [Date] date)''')
+
+
 
 conn.commit()
