3c9994147723e849ca2ce194479c7db3c60d8e52
Łukasz Kępka <lukaszkepka97@gmail.com>
2021-04-21 21:47:46 +0200

    Added adding to db

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ import sqlite3
 conn = sqlite3.connect('Logs.db')
 
 c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS LOGS
-             ([generated_id] INTEGER PRIMARY KEY,[Text] text, [Server_ID] integer, [Date] date)''')
+c.execute('''
+            CREATE TABLE IF NOT EXISTS Logs(
+            message text,
+            event_type text,
+            server_id integer,
+            date date
+            )''')
 
+file1 = open('logs.txt', 'r')
+while True:
+    line = file1.readline()
+    if not line:
+        break
 
-
+    splitLine = line.strip().split(' ')
+    day = splitLine[0]
+    hour = splitLine[1]
+    event = splitLine[2]
+    text = ''.join(splitLine[3:]).replace(':', '').replace('.', '')
+    formattedDate = '2021-{}-{} {}'.format(day.split('/')[0], day.split('/')[1], hour)
+    print(day, hour, event, formattedDate, text)
+    sql = 'INSERT INTO Logs(message, event_type, server_id, date) VALUES(?, ?, 1, ?);'
+    c.execute(sql, [text, event, formattedDate])
+file1.close()
 conn.commit()
