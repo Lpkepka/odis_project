bdc48cbb101f53104fe2dc8b0e1ad3c8a022f8e1
Łukasz Kępka <lukaszkepka97@gmail.com>
2021-04-26 19:56:35 +0200

    added requests, threading and db insert

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
 import sqlite3
+import threading
+import requests
 
 conn = sqlite3.connect('Logs.db')
-
 c = conn.cursor()
-c.execute('''
-            CREATE TABLE IF NOT EXISTS Logs(
-            message text,
-            event_type text,
-            server_id integer,
-            date date
-            )''')
 
-file1 = open('logs.txt', 'r')
-while True:
-    line = file1.readline()
-    if not line:
-        break
+def importConfiguration():
+    configurationFile = open('logs.txt', 'r')
+    while True:
+        line = configurationFile.readline()
+        if not line:
+            break
+        splitLine = line.split(" ")
+        globalConfiguration[splitLine[0]] = splitLine[1]
 
-    splitLine = line.strip().split(' ')
-    day = splitLine[0] #format -> MM/dd
-    hour = splitLine[1] #format -> HH:mm:ss
-    event = splitLine[2]
-    text = ''.join(splitLine[3:]).replace(':', '').replace('.', '') #removes all the ;..... from the front
-    formattedDate = '2021-{}-{} {}'.format(day.split('/')[0], day.split('/')[1], hour)
+    configurationFile.close()
 
-    sql = 'INSERT INTO Logs(message, event_type, server_id, date) VALUES(?, ?, 1, ?);'
-    c.execute(sql, [text, event, formattedDate])
+def insertDataToDB(dataDictionary, serverURL):
+    for line in dataDictionary:
+        splitLine = line.strip().replace('- - ', '').split(' ')
+        ip_addr = splitLine[0]
+        date = splitLine[1].replace('[', '').replace(']', '')
+        method = splitLine[2]
+        path = splitLine[3]
+        http_version = splitLine[4]
+        response_code = splitLine[5]
 
-file1.close()
-conn.commit()
+        sql = 'INSERT INTO Logs(ip_addr,server_url,method,path,response_code,http_version,date) VALUES(?, ?, ?, ?, ?, ?, ?);'
+        c.execute(sql, [ip_addr, serverURL, method, path, response_code, http_version, date])
+    conn.commit()
+
+
+def fetchNewData(interval, url):
+    threading.Timer(interval, fetchNewData).start()
+    response = requests.get(url)
+    insertDataToDB(response.json()["Logs"], url)
+
+def launchAPICalls():
+    for url, timeoutValue in globalConfiguration.items():
+        fetchNewData(timeoutValue, url)
+
+
+globalConfiguration = {}
+importConfiguration()
+
+c.execute('''CREATE TABLE IF NOT EXISTS Logs(
+            ip_addr text,
+            server_url text,
+            method text,
+            path text,
+            response_code text,
+            http_version text,
+            date date)''')
+
+launchAPICalls()
