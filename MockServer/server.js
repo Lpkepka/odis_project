21cafc7026d38058eb0040b92a08a39b93af9adf
Patrycja Biel <bielpatrycjaa@gmail.com>
2021-04-26 18:47:29 +0200

    Add mock server for testing purposes

---
+++ MockServer/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get("/api", function(req, res)  { 
+
+        var fs = require('fs');
+        fs.readFile('log.txt', 'utf8', function(err,data) {
+            if(err) throw err;
+            let splitted = data.toString().replace(/"/g, '').split("\n");
+            var json = JSON.parse('{"Logs" : "' + splitted.toString() + '"}');
+            console.log(json);
+            res.json(json) 
+        });
+        
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
