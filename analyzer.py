67566820a141341a27234d53fb3256ade692967b
Łukasz Kępka <lukaszkepka97@gmail.com>
2021-05-03 18:07:41 +0200

    Added multiple values to query and reload config every hit

--- analyzer.py
+++ analyzer.py
@@ -27,20 +27,39 @@ def buildSqlQuery(configuration, configurationRows):
     dataArray = configuration[-4:-2]
     isPositive = configuration[-2] == '1'
     count = int(configuration[-1])
-    for i, condition in enumerate(cutOffArray):
-        if condition != '-':
+    for i, conditions in enumerate(cutOffArray):
+        if conditions != '-':
             equal = '' if isPositive else 'NOT '
-            conditionString = " {}{}='{}' AND".format(equal, configurationRows[i], condition)
-            conditionalQuery += conditionString
+            orOrAnd = 'OR ' if isPositive else 'AND '
+            conditionsSplit = conditions.split(';')
+            if len(conditionsSplit) > 1 and isPositive:
+                conditionalQuery += ' ('
+            for condition in conditionsSplit:
+                conditionString = " {}{}='{}' {}".format(equal, configurationRows[i], condition, orOrAnd)
+                conditionalQuery += conditionString
+            if len(conditionsSplit) > 1 and isPositive:
+                conditionalQuery += ')'
+        if conditionalQuery[-4:] == 'OR )':
+            conditionalQuery = ') AND '.join(conditionalQuery.rsplit('OR )', 1))
+        elif conditionalQuery[-3:] == 'OR ':
+            conditionalQuery = ' AND '.join(conditionalQuery.rsplit('OR ', 1))
 
     if ((dataArray[0] != '-') and (dataArray[1] != '-')):
-        datetime_object = datetime.strptime(dataArray[0], '%d/%b/%Y:%I:%M:%S%z')
-        time_change = getTimeDeltaFor(dataArray[1])
-        new_time = datetime_object + time_change
-        new_time_string = new_time.strftime('%d/%b/%Y:%I:%M:%S%z')
-        conditionalQuery += " date BETWEEN '{}' AND '{}'".format(dataArray[0], new_time_string)
+        splitDates = dataArray[0].split(';')
+        if len(splitDates) > 1:
+            conditionalQuery += '('
+        for date in splitDates:
+            datetime_object = datetime.strptime(date, '%d/%b/%Y:%I:%M:%S%z')
+            time_change = getTimeDeltaFor(dataArray[1])
+            new_time = datetime_object + time_change
+            new_time_string = new_time.strftime('%d/%b/%Y:%I:%M:%S%z')
+            conditionalQuery += " date BETWEEN '{}' AND '{}' OR".format(date, new_time_string)
     else:
         conditionalQuery = conditionalQuery[:-4]
+
+    conditionalQuery = conditionalQuery[:-3]
+    if len(splitDates) > 1:
+        conditionalQuery += ')'
 
     finalQueryString = baseQuery.format(conditionalQuery)
     finalQueryString += ';'
@@ -59,7 +78,7 @@ def importAnalyzerConfiguration():
             else:
                 if row[0] == 'if':
                     query = buildSqlQuery(row[1:], configurationRows)
-                    numberOfResults = exectureSQLQuery(query[0])
+                    numberOfResults = exectureSQLQuery(query)
                     ifCount += 1
                     goingToElse = numberOfResults > query[1]
                 elif row[0] == 'else':
@@ -76,21 +95,22 @@ def importAnalyzerConfiguration():
             line_count += 1
 
 def exectureSQLQuery(query):
+    print(query[0])
     c.execute(query[0])
     rows = c.fetchall()
     return int(rows[0][0])
 
 def executeSQLQueries():
     for query in sqlSelectors:
-        rowCount = exectureSQLQuery(query[0])
-        print('------------------- Raport ------------------')
-        print('Found {} rows matching data from configuration'.format(rowCount))
-        print('Expected less than {} rows'.format(query[1]))
-        print('------------------------------------- \n\n')
-
-
-importAnalyzerConfiguration()
+        rowCount = exectureSQLQuery(query)
+        if rowCount > query[1]:
+            print('------------------- Raport ------------------')
+            print('Found {} rows matching data from configuration'.format(rowCount))
+            print('Expected less than {} rows'.format(query[1]))
+            print('------------------------------------- \n\n')
 
 while True:
+    sqlSelectors.clear()
+    importAnalyzerConfiguration()
     executeSQLQueries()
     time.sleep(10)
