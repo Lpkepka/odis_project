3077690acc97a8b86b89a9dc034bfa5791ebbf77
Patrycja Biel <bielpatrycjaa@gmail.com>
2021-05-22 18:44:51 +0200

    Add tests for different types of logs

--- Tests/logParserTests.py
+++ Tests/logParserTests.py
@@ -8,6 +8,8 @@ class TestParser(unittest.TestCase):
     IISLog = '192.168.114.201, -, 03/20/05, 7:55:20, W3SVC2, SERVER, 172.21.13.45, 4502, 163, 3223, 200, 0, GET, /DeptLogo.gif, -,'
     HTTPLog = 'GET /tutorials/other/top-20-mysql-best-practices/ HTTP/1.1'
     ApacheLog = '127.0.0.1 – frank [10/Oct/2000:13:55:36 -0700] “GET /apache_pb.gif HTTP/1.0” 200 2326'
+    NginxLog = '127.0.0.1 - dbmanager [20/Nov/2017:18:52:17 +0000] "GET / HTTP/1.1" 401 188 "-" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"'
+    NCAS_access_log = '172.21.13.45 - Microsoft\JohnDoe [07/Apr/2004:17:39:04 -0800] "GET /scripts/iisadmin/ism.dll?http/serv HTTP/1.0" 200 3401'
     invalidLog = 'asdklsajdklajsd'
 
     def test_apache_log(self):
@@ -38,9 +40,23 @@ class TestParser(unittest.TestCase):
         self.assertEqual(values[2], 'https://www.example.com:443/')
         self.assertEqual(values[3], '200')
 
+    def test_NginxLog(self):
+        values = parser.parseLogs(self.NginxLog)
+        self.assertEqual(len(values), 6)
+        self.assertEqual(values[0], '127.0.0.1')
+        self.assertEqual(values[2], '/')
+        self.assertEqual(values[3], '401')
+
+    def test_NCAS_access_Log(self):
+        values = parser.parseLogs(self.NCAS_access_log)
+        self.assertEqual(len(values), 6)
+        self.assertEqual(values[0], '172.21.13.45')
+        self.assertEqual(values[2], '/scripts/iisadmin/ism.dll?http/serv')
+        self.assertEqual(values[3], '200')
+
     def test_invalid_logs(self):
         values = parser.parseLogs(self.invalidLog)
-        self.assertEqual(values, None)
+        self.assertIsNone(values)
 
 if __name__ == '__main__':
     unittest.main()
